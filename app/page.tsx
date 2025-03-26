09dc466b2a038c712ae37980c02fdd4ca22afa65
dayuan.jiang <jdy.toh@gmail.com>
2025-03-26 06:43:27 +0000

    minor: simplify DiagramPageLayout structure by removing unnecessary divs

--- app/page.tsx
+++ app/page.tsx
@@ -10,23 +10,17 @@ function DiagramPageLayout({ children }: { children: React.ReactNode }) {
 
     return (
         <div className="flex h-screen bg-gray-100">
-            <div className="w-2/3 p-1 h-full">
-                <div className="h-full relative">
-                    <div className="absolute inset-0">
-                        <div className="w-full h-full">
-                            <DrawIoEmbed
-                                ref={drawioRef}
-                                onExport={handleDiagramExport}
-                                urlParameters={{
-                                    spin: true,
-                                    libraries: false,
-                                    saveAndExit: false,
-                                    noExitBtn: true,
-                                }}
-                            />
-                        </div>
-                    </div>
-                </div>
+            <div className="w-2/3 p-1 h-full relative">
+                <DrawIoEmbed
+                    ref={drawioRef}
+                    onExport={handleDiagramExport}
+                    urlParameters={{
+                        spin: true,
+                        libraries: false,
+                        saveAndExit: false,
+                        noExitBtn: true,
+                    }}
+                />
             </div>
             <div className="w-1/3 h-full p-1">{children}</div>
         </div>
